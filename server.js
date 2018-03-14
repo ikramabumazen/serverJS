e6d06e563650bf169566c9e79d37294163488e17
Your Name <you@example.com>
2018-03-14 17:56:02 +0200

    server.js __ read drugs.json file

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ var words=JSON.parse(data);
 var bodyparser=require('body-parser');
 console.log(words);
 
-
+var d=fs.readFileSync('drugs.json', 'utf8');
+var drug=JSON.parse(d);
+var bodyparser=require('body-parser');
+console.log(drug);
