259d5c31288b6bbbfb07265f946cc6d083f3aa0f
Your Name <you@example.com>
2018-03-14 17:57:50 +0200

    server.js __ install and include expressjs

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ var d=fs.readFileSync('drugs.json', 'utf8');
 var drug=JSON.parse(d);
 var bodyparser=require('body-parser');
 console.log(drug);
+
+
+var express=require('express');
+var app=express();
+var server=app.listen(5013 ,listening);
+
+function listening(){
+console.log("listening..");
+}
+
+app.use(express.static('website'));
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
